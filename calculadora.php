4b895383b2d0dac9cfbda759a446e06537331d12
Everson de Freitas <137961764+eversonpf@users.noreply.github.com>
2023-08-01 21:54:09 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -28,5 +28,52 @@ if ($operador == "/"){
 echo $resultado;
 
 
+<?php
+
+$value = 100;
+$value2 = 10;
+$operador = 'perc+';
+
+$resultado = 0;
+
+
+if ($operador == '+'){
+    $resultado = $value + $value2;
+}
+
+if ($operador == '-'){
+    $resultado = $value - $value2;
+}
+
+if ($operador == '*'){
+    $resultado = $value * $value2;
+
+}
+
+if ($operador == '/'){
+    $resultado = $value / $value2;
+
+}
+
+if ($operador == '^'){
+    $resultado = pow($value, $value2);
+    $resultado = $value ** $value2;
+
+}
+
+ if ($operador == 'perc'){
+     $desconto = ($value * $value2)  / 100;  //10
+     $resultado = $value / - $desconto;
+}
+
+if ($operador == 'perc+'){
+    $acrescimo= ($value * $value2) / 100;
+    $resultado = $value + $acrescimo;
+}
+
+    echo "o valor é {$resultado}";
+
+
+
 
 
