50ac8fbef764d906bd826e2ba08e8254613a32bc
Everson de Freitas <137961764+eversonpf@users.noreply.github.com>
2023-08-22 21:38:14 -0300

    calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -1,148 +1,132 @@
 <?php
 
-$value = '300';
-$value2 = "500";
-$operador ='+';
+// $value = 100;                  
+// $value2 = 10;
+// $operador = 'perc+';
 
-$resultado = 0;
-
-if ($operador == "+"){
-    $resultado = $value + $value2;
-}
+// $resultado = 0;
 
 
-if ($operador == "-"){
-    $resultado = $value - $value2;
-}
+// if ($operador == '+'){
+//     $resultado = $value + $value2;
+// }
+
+// if ($operador == '-'){
+//     $resultado = $value - $value2;
+// }
+
+// if ($operador == '*'){
+//     $resultado = $value * $value2;
+
+// }
+
+// if ($operador == '/'){
+//     $resultado = $value / $value2;
+
+// }
+
+// if ($operador == '^'){
+//     $resultado = pow($value, $value2);
+//     $resultado = $value ** $value2;
+
+// }
+
+//  if ($operador == 'perc'){
+//      $desconto = ($value * $value2)  / 100;  //10
+//      $resultado = $value / - $desconto;
+// }
+
+// if ($operador == 'perc+'){
+//     $acrescimo= ($value * $value2) / 100;
+//     $resultado = $value + $acrescimo;
+// }
+
+// //    echo "o valor é {$resultado}";
+
+// //
 
 
-if ($operador == "*"){
-    $resultado = $value * $value2;
-}
+// $media =6;
+
+// $prova = 4.8;
+// $peso = 5;
+// $prova2 = 1.5;
+// $peso2 = 3;
+// $prova3 = 3;
+// $peso3 = 2;
+// $numerador = ($prova * $peso) + ($prova2 * $peso2) + ($prova3 * $peso3);
+// $denominador = ($peso + $peso2 + $peso3);
+// $MP = $numerador / $denominador;
+
+// $mp= $numerador / $denominador;
+
+//    if ($mp >= $media ) { 
+//    echo " O aluno foi reprovado {$MP}";
+
+//    $numero = 1;
+//    $contador = 0;
+
+//    for ($i = 1; $i <= $numero ; $i ++)
 
 
-if ($operador == "/"){
-    $resultado = $value / $value2;
-}
+//    if ($numero % $si ==0)
 
-echo $resultado;
+// {
 
+//      $contador ++;# code ...
 
-<?php
+// } #code ...
+//    }
 
-$value = 100;
-$value2 = 10;
-$operador = 'perc+';
+//     if($contador ==2)
 
-$resultado = 0;
+// {
 
+//    echo "primo "; # code...
+// }
 
-if ($operador == '+'){
-    $resultado = $value + $value2;
-}
+//     else
 
-if ($operador == '-'){
-    $resultado = $value - $value2;
-}
+// {
+//     echo "nao primo";
 
-if ($operador == '*'){
-    $resultado = $value * $value2;
-
-}
-
-if ($operador == '/'){
-    $resultado = $value / $value2;
-
-}
-
-if ($operador == '^'){
-    $resultado = pow($value, $value2);
-    $resultado = $value ** $value2;
-
-}
-
- if ($operador == 'perc'){
-     $desconto = ($value * $value2)  / 100;  //10
-     $resultado = $value / - $desconto;
-}
-
-if ($operador == 'perc+'){
-    $acrescimo= ($value * $value2) / 100;
-    $resultado = $value + $acrescimo;
-}
-
-    echo "o valor é {$resultado}";
-
-<?php
-
-$value = 100;                  
-$value2 = 10;
-$operador = 'perc+';
-
-$resultado = 0;
+// }
 
 
-if ($operador == '+'){
-    $resultado = $value + $value2;
-}
+    $acrecimos = 0; 
+    $horaInicial = '16:00:00';
+    $horaFinal = '17:30:00';
 
-if ($operador == '-'){
-    $resultado = $value - $value2;
-}
-
-if ($operador == '*'){
-    $resultado = $value * $value2;
-
-}
-
-if ($operador == '/'){
-    $resultado = $value / $value2;
-
-}
-
-if ($operador == '^'){
-    $resultado = pow($value, $value2);
-    $resultado = $value ** $value2;
-
-}
-
- if ($operador == 'perc'){
-     $desconto = ($value * $value2)  / 100;  //10
-     $resultado = $value / - $desconto;
-}
-
-if ($operador == 'perc+'){
-    $acrescimo= ($value * $value2) / 100;
-    $resultado = $value + $acrescimo;
-}
-
-//    echo "o valor é {$resultado}";
-
-//
-
-  
-
-$media =6;
-
-$prova = 4.8;
-$peso = 5;
-$prova2 = 1.5;
-$peso2 = 3;
-$prova3 = 3;
-$peso3 = 2;
-$numerador = ($prova * $peso) + ($prova2 * $peso2) + ($prova3 * $peso3);
-$denominador = ($peso + $peso2 + $peso3);
-$MP = $numerador / $denominador;
-
-$mp= $numerador / $denominador;
-
-if ($mp >= $media ) { 
-   echo " O aluno foi reprovado {$MP}";
-  
-
-}
+    $arrHora = explode(":",$horaInicial);
+    $hora = $arrHora[0];
+    $minutos = $arrHora[1];
+    $segundos = $arrHora[2];
 
 
+    $horaEmSegundos = $hora * 3600;
+    $minutosEmSegundos = $minutos * 60;
+
+    $totalInicialEmSegundos = $horaEmSegundos + $minutosEmSegundos + $segundos;
+
+    $arrHora = explode(":", $horaFinal);
+    $horaFinal = $arrHora[0];
+    $minutosFinal = $arrHora[1];
+    $segundosFinal = $arrHora[2];
+
+    $horaFinalEmSegundos= $horaFinal * 3600;
+    $minutosFinalEmSegundos= $minutosFinal * 60;
+
+    $totalFinalEmSegundos = $horaFinalEmSegundos + $minutosFinalEmSegundos + $segundosFinal;
+
+    $tempoDoJogo = ($totalFinalEmSegundos - $totalInicialEmSegundos) /60;
+
+     echo "tempo do jogo {$tempoDoJogo}";
+
+     //Tempo da partida
+
+     $acrecimos = 5;
+     $horaInicial = '23:00:00';
+     $horaFinal = '00:30:00';
 
 
 
